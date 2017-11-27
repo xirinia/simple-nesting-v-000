b7167dc2f7345e4d71a39ea073f3fc598ba94f97
Anaxirinia Jimenez <anax131@gmail.com>
2017-11-27 04:42:16 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_ritchie]
+		 programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -67,6 +67,10 @@ def adding_matz
 # }
 
 	programmer_hash =
+				:yukihiro_matsumoto => {
+			  	:known_for => "Ruby",
+			    :languages => ["LISP", "C"]
+		},
  		{
         :grace_hopper => {
           :known_for => "COBOL",

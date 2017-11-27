c76ec33a1b34081ec95006134f2748133c9ddab3
Anaxirinia Jimenez <anax131@gmail.com>
2017-11-27 04:48:39 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -107,7 +107,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
-		 [programmer_hash][:alan_kay][:known_for] = alans_new_info
+		 programmer_hash[:alan_kay][:known_for] = alans_new_info
 
 
 
@@ -129,6 +129,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-		 [programmer_hash][:dennis_ritchie][:languages] << "Assembly"
+		 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 
 end
